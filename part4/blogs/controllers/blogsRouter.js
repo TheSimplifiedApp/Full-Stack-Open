343c3c2b61bc6c07d099fd4199d5b7a36fbdd88e
TheSimplifiedApp <72549339+TheSimplifiedApp@users.noreply.github.com>
2023-01-15 19:04:17 +0800

    Part 4 blogList app

---
+++ part4/blogs/controllers/blogsRouter.js
@@ -0,0 +1,51 @@
+const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  // verify token
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken) {
+    return response.status(401).json({ error: 'token missing or invalid'})
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const { title, author, url, likes } = request.body
+  if (!title || !url) return response.status(400).json({ error: "title or url missing." })
+  const blog = new Blog({ title, author: author || "", url, likes: likes || 0, user: user.id })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) return response.status(404).json({error: 'blog does not exist'})
+
+  const creater = request.user
+
+  if (blog.user.toString() === creater) {
+    await Blog.findByIdAndRemove(request.params.id)
+    return response.status(204).end()
+  }
+
+  return response.status(401).json({ error: 'only creater can delete the blog'})
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+  response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
